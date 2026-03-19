babc16eb62b4af5c7a6d6fcbb59ba357014fa8d0
jardmclan <mcleanj@hawaii.edu>
2026-03-18 17:26:13 -1000

    set bash flags

--- containers/day/task.sh
+++ containers/day/task.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -euo pipefail
+
 echo "[task.sh] [1/4] Starting Execution."
 export TZ="HST"
 echo "It is currently $(date)."
-if [ $CUSTOM_DATE ]; then
+if [[ -v CUSTOM_DATE ]]; then
     echo "An aggregation date was provided by the environment."
 else
     export CUSTOM_DATE=$(date -d "1 day ago" --iso-8601)
